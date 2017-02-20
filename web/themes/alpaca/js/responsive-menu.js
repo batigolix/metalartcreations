35352f6c78c8e739cf7f4763f2677a6ada96973a
Boris Doesborg <bdoesborg@gmail.com>
2017-02-20 17:47:51 +0100

    code sniffing the alpaca theme

--- web/themes/alpaca/js/responsive-menu.js
+++ web/themes/alpaca/js/responsive-menu.js
@@ -1,9 +1,16 @@
-/* Toggles between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/**
+ * @file
+ * Provides responsive menu script.
+ */
+
+// Toggles between adding and removing the "responsive" class to topnav when
+// the user clicks on the icon.
 function openResponsiveMenu() {
   var x = document.getElementById("responsive-menu");
   if (x.className === "topnav") {
     x.className += " responsive";
-  } else {
+  }
+  else {
     x.className = "topnav";
   }
 }
