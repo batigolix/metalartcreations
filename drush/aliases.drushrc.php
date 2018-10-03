5cd781bd7976215958a88ac69718bb11b84e2a95
Boris Doesborg <boris.doesborg@finalist.nl>
2018-10-03 23:30:57 +0200

    aliases

---
+++ drush/aliases.drushrc.php
@@ -0,0 +1,15 @@
+<?php
+
+$aliases['dev'] = array(
+  'uri' => 'mac.test',
+  'root' => '/var/www/html/web',
+);
+
+$aliases['test'] = array(
+  'uri' => 'mac-build.test',
+  'root' => '/var/www/html/web',
+);
+
+
+
+
