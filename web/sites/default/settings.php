0199b02a8b5e9097fbdffa015f9bd80b6cf35e49
Boris Doesborg <boris.doesborg@finalist.nl>
2018-12-06 21:39:22 +0100

    update drupal core

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -763,6 +763,6 @@ if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
 
-$settings['install_profile'] = 'standard';
+#$settings['install_profile'] = 'standard';
 //$config_directories['sync'] = 'sites/default/files/config_OmpzcnhSHzIY-lcb9ZqAVpp-hixEVZCB5n8VKVh0KnK5ir6jBge4cLcUs3Mw0Sgiv09-BcGBPw/sync';
 $config_directories['sync'] = '../config/sync';
